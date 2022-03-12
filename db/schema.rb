140fe065f969763a5435a0122973adea4e3eed3c
PlaustralCL <plaustralcl@gmail.com>
2022-03-11 20:22:26 -0500

    Set up basic Post data entry and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_11_113210) do
+ActiveRecord::Schema.define(version: 2022_03_11_114118) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
